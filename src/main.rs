d5fdd88a017e200daf48cb57713b0aebc32a3cf3
maeechen <maeechen@tencent.com>
2021-08-04 13:32:29 +0800

    Add process terminator

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 
+use std::{io::{self, stdin}, process, thread};
+
 use auto::TetrisAuto;
 use game_play::Game;
+use termion::{event::Key, input::TermRead};
 
 pub mod grid;
 pub mod game;
@@ -16,5 +19,16 @@ pub mod utils;
 fn main() {
     let game = Game::new();
     // game.start();
+
+    thread::spawn(|| {
+        let keys = stdin().keys();
+        for key in keys {
+            match key {
+                Ok(Key::Ctrl('c')) => process::exit(0),
+                _ => (),
+            }
+        }
+    });
+
     TetrisAuto::start();
 }
